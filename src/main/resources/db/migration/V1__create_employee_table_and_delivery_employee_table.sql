a41fd07cfac9710f27c8d5aec8d5e5f071fdddbb
jamesliambutler <james.liam.butler@kainos.com>
2024-11-06 09:50:56 +0000

    YJ-001_Create_db_migration_for_employee_and_delivery_employee_tables

---
+++ src/main/resources/db/migration/V1__create_employee_table_and_delivery_employee_table.sql
@@ -0,0 +1,15 @@
+Employee (
+	employeeId int NOT NULL AUTO_INCREMENT,
+	firstName varchar(50),
+	lastName varchar(50),
+	salary decimal(7, 2),
+	bankAccountNumber varchar(34),
+	nationalInsuranceNumber varchar(9),
+	PRIMARY KEY(employeeId)
+);
+
+DeliveryEmployee (
+	employeeId int NOT NULL,
+	PRIMARY KEY(employeeId),
+	FOREIGN KEY (employeeId) REFERENCES Employee(employeeId)
+);
