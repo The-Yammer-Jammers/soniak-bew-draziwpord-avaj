49a8c06e523f0b2406e0bb96fdefbbbe02503b05
Matthew Jordan <matthew.jordan@kainos.com>
2024-11-07 10:50:44 +0000

    YJ_004: Fixed missmatched indentation

--- src/main/resources/db/migration/V7__create_auth_tables.sql
+++ src/main/resources/db/migration/V7__create_auth_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `Role`(
-	RoleID TINYINT NOT NULL,
+    RoleID TINYINT NOT NULL,
     Name varchar(64) NOT NULL,
     PRIMARY KEY (RoleID)
 );
@@ -9,7 +9,7 @@ INSERT INTO `Role` VALUES (2, 'Sales');
 INSERT INTO `Role` VALUES (3, 'Management');
 
 CREATE TABLE `User` (
-	Username varchar(64) NOT NULL,
+    Username varchar(64) NOT NULL,
     Password varchar(64) NOT NULL,
     RoleID TINYINT NOT NULL,
     PRIMARY KEY (Username),
